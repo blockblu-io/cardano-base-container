a9b4d0b6239f0f5ad17013cbd710a241fc9dfd27
Kevin Haller <staking@outofbits.com>
2021-04-28 20:39:52 +0200

    Replaces the build scripts with a Makefile.

--- GHC/create-manifest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker manifest create "adalove/ubuntu:$1-ghc$2-c$3" \
-	"adalove/ubuntu:$1-ghc$2-c$3_amd64" \
-	"adalove/ubuntu:$1-ghc$2-c$3_arm" \
-	"adalove/ubuntu:$1-ghc$2-c$3_arm64"
-
-docker manifest push --purge "adalove/ubuntu:$1-ghc$2-c$3"
